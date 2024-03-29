a137546ef6bd6202e4a339baf1b182a2a2bf1ece
Ekaterina Scherbakova <kat-m1@mail.ru>
2024-03-29 20:10:57 +0500

    Add lection 5, json

---
+++ lection_5/json.js
@@ -0,0 +1,22 @@
+const data = `
+{
+  "sammy": {
+    "username": "SammyShark",
+    "location": "Indian Ocean",
+    "online": true,
+    "followers": 987
+  },
+  "jesse": {
+    "username": "JesseOctopus",
+    "location": "Pacific Ocean",
+    "online": false,
+    "followers": 432
+  },
+  "drew": {
+    "username": "DrewSquid",
+    "location": "Atlantic Ocean",
+    "online": false,
+    "followers": 321
+  }
+}
+`;
